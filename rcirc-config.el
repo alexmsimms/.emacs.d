ba9e28051af1222aa691000deb0904dc03cfb3c5
Alex Simms <alexmsimms@gmail.com>
2014-06-03 00:52:04 -0400

    IRC error settings, yeah, but not loading automatically, have to manually execute the lisp.

---
+++ rcirc-config.el
@@ -0,0 +1,6 @@
+(setq rcirc-default-nick "Alonzo-Emacs")
+(setq rcirc-default-user-name "Alonzo")
+(setq rcirc-default-full-name "Alan Onzo")
+(setq rcirc-server-alist
+      '(("irc.rizon.net" :port 6697 :encryption tls
+         :channels ("#answertolife"))))
