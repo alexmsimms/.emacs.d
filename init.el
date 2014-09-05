5d771025d2f8ac84c02d68dd039753321f7348a3
Alex Simms <asimms1@cs.swarthmore.edu>
2014-09-04 21:39:25 -0400

    Add theme, smart mode line

--- init.el
+++ init.el
@@ -16,7 +16,9 @@
 (defvar packages-list
   '(helm
     magit
-    nyan-mode)
+    nyan-mode
+    smart-mode-line
+    monokai-theme)
   "List of packages needs to be installed at launch")
 
 (defun has-package-not-installed ()
@@ -34,9 +36,13 @@
       (package-install p))))
 
 ;; UI Enhancements ;;
-(set-default-font "Source Code Pro 12")
+;;(set-default-font "Source Code Pro 12")
+(load-theme 'monokai t)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
+
+(sml/setup)
+(sml/apply-theme 'automatic)
 
 (setq mac-option-key-is-meta nil)
 (setq mac-command-key-is-meta t)
@@ -109,4 +115,17 @@
 ;; Helm Set ;;
 
 ;; Nyan Mode ;;
+(setq nyan-bar-length 16)
 (nyan-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
