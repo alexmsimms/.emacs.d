d5c6576fb4c142d3e4372bff8d422fb77f838c73
Alex Simms <alexmsimms@gmail.com>
2014-09-11 10:12:57 -0400

    Have some customization

--- init.el
+++ init.el
@@ -36,21 +36,42 @@
     (when (not (package-installed-p p))
       (package-install p))))
 
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+
+
 ;; UI Enhancements ;;
 ;;(set-default-font "Source Code Pro 12")
 (load-theme 'monokai t)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
-(setq indicate-empty-lines t)
+(setq-default indicate-empty-lines t)
 (show-paren-mode 1)
+(column-number-mode 1)
+
 
 (sml/setup)
 (sml/apply-theme 'automatic)
 
-(setq mac-option-key-is-meta nil)
-(setq mac-command-key-is-meta t)
-(setq mac-command-modifier 'meta)
-(setq mac-option-modifier nil)
+(setq mac-option-key-is-meta nil
+      mac-command-key-is-meta t
+      mac-command-modifier 'meta
+      mac-option-modifier nil)
 
 (setq inhibit-startup-screen t)
 
@@ -120,18 +141,6 @@
 ;; Nyan Mode ;;
 (setq nyan-bar-length 16)
 (nyan-mode)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
+(setq-default ispell-program-name "/usr/local/bin/aspell")
+(setq-default ispell-list-command "list")
