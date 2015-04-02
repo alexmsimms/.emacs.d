2e5202091d25b3a34364a87bb5755478e3351ca1
Alex Simms <alexmsimms@gmail.com>
2015-04-01 20:55:21 -0400

    Remove clean mode line thing

--- init.el
+++ init.el
@@ -119,40 +119,39 @@
 (sml/apply-theme 'automatic)
 (rich-minority-mode)
 
-
-;; From the "mastering emacs" guy
-(defvar mode-line-cleaner-alist
-  `((yas/minor-mode . " υ")
-    (paredit-mode . " π")
-    (eldoc-mode . "")
-    (abbrev-mode . "")
-    ;; Major modes
-    (lisp-interaction-mode . "λ")
-    (emacs-lisp-mode . "EL")
-	(python-mode . "PY" )
-	(pyret-mode . "ARR"))
-  "Alist for `clean-mode-line'.
+(when nil
+  ;; From the "mastering emacs" guy
+  (defvar mode-line-cleaner-alist
+	`((yas/minor-mode . " υ")
+	  (paredit-mode . " π")
+	  (eldoc-mode . "")
+	  (abbrev-mode . "")
+	  ;; Major modes
+	  (lisp-interaction-mode . "λ")
+	  (emacs-lisp-mode . "EL")
+	  (python-mode . "PY" )
+	  (pyret-mode . "ARR"))
+	"Alist for `clean-mode-line'.
 
 When you add a new element to the alist, keep in mind that you
 must pass the correct minor/major mode symbol and a string you
 want to use in the modeline *in lieu of* the original.")
 
 
-(defun clean-mode-line ()
-  (interactive)
-  (loop for cleaner in mode-line-cleaner-alist
-        do (let* ((mode (car cleaner))
-                 (mode-str (cdr cleaner))
-                 (old-mode-str (cdr (assq mode minor-mode-alist))))
-             (when old-mode-str
+  (defun clean-mode-line ()
+	(interactive)
+	(loop for cleaner in mode-line-cleaner-alist
+		  do (let* ((mode (car cleaner))
+					(mode-str (cdr cleaner))
+					(old-mode-str (cdr (assq mode minor-mode-alist))))
+			   (when old-mode-str
                  (setcar old-mode-str mode-str))
                ;; major mode
-             (when (eq mode major-mode)
-               (setq mode-name mode-str)))))
+			   (when (eq mode major-mode)
+				 (setq mode-name mode-str)))))
 
 
-(add-hook 'after-change-major-mode-hook 'clean-mode-line)
-
+  (add-hook 'after-change-major-mode-hook 'clean-mode-line))
 (setq inhibit-startup-screen t)
 
 ;; UI Enhanced ;;
